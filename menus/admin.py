c75f6b62f9da62041159868468ab688e4f19a347
Mir-Yuchi <mirazimov.kr@gmail.com>
2025-06-17 00:36:29 +0800

    feat(admin): register MenuItem in Django admin

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MenuItem
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    """
+    Admin interface for managing MenuItem objects.
+    """
+
+    list_display = ("title", "menu_name", "parent", "order")
+    list_filter = ("menu_name",)
+    search_fields = ("title", "url")
+    ordering = ("menu_name", "parent__id", "order")
+    list_editable = ("order",)
+    raw_id_fields = ("parent",)
